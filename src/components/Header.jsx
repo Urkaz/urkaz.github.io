f2ce3bdf9a1bd95a1561b2e75d44e8c2c318b7a4
Urkaz <mrurkaz@gmail.com>
2025-02-21 04:16:00 +0100

    Header block with less scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header({ children }) {
       const selectBody = document.querySelector('body');
       const selectHeader = document.querySelector('#header');
       if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-      window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+      window.scrollY > 50 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
     }
     document.addEventListener('scroll', toggleScrolled);
     window.addEventListener('load', toggleScrolled);
