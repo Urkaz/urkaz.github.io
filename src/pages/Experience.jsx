4f156f243e918808aaaa870a059c979650428f5c
Urkaz <mrurkaz@gmail.com>
2025-02-21 04:19:03 +0100

    WIP Experience section

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,27 +1,74 @@
 import React from "react";
 
+import SectionContent from "../components/SectionsContent.jsx";
+
 const Experience = () => {
     return (
-        <div className="page-title" data-aos="fade">
-            <div className="heading">
-                <div className="container">
-                    <div className="row d-flex justify-content-center text-center">
-                        <div className="col-lg-8">
-                            <h1>Experience</h1>
-                            <p className="mb-0">Odio et unde deleniti. Deserunt numquam exercitationem. Officiis quo odio sint voluptas consequatur ut a odio voluptatem. Sit dolorum debitis veritatis natus dolores. Quasi ratione sint. Sit quaerat ipsum dolorem.</p>
+        <>
+            <SectionContent title="Experience" description="<TODO>" sectionId="experience">
+                <div className="row">
+                    {/*Left side */}
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+                        <h3 className="experience-title">Education</h3>
+
+                        <div className="experience-item">
+                            <h4>Master of Fine Arts &amp; Graphic Design</h4>
+                            <h5>2015 - 2016</h5>
+                            <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+                            <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
+                        </div>
+
+                        <div className="experience-item pb-0">
+                            <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
+                            <h5>2010 - 2014</h5>
+                            <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
+                            <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
+                        <h3 className="experience-title">Professional Experience</h3>
+
+                        <div className="experience-item">
+                            <h4>Unreal Programmer & Porting Engineer</h4>
+                            <h5>2019 - Present</h5>
+                            <p><em>Experion, New York, NY </em></p>
+                            <ul>
+                                <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
+                                <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
+                                <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
+                                <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
+                            </ul>
+                        </div>
+
+                        <div className="experience-item">
+                            <h4>QA Technician</h4>
+                            <h5>2019 - Present</h5>
+                            <p><em>Experion, New York, NY </em></p>
+                            <ul>
+                                <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
+                                <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
+                                <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
+                                <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
+                            </ul>
+                        </div>
+
+                        <div className="experience-item pb-0">
+                            <h4>QA Tester Internship</h4>
+                            <h5>2017 - 2018</h5>
+                            <p><em>Lollipop Robot</em></p>
+                            <ul>
+                                <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
+                                <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+                                <li>Recommended and consulted with clients on the most appropriate graphic design</li>
+                                <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
-            </div>
-            <nav className="breadcrumbs">
-                <div className="container">
-                    <ol>
-                        <li><a href="index.html">Home</a></li>
-                        <li className="current">Experience</li>
-                    </ol>
-                </div>
-            </nav>
-        </div>
+            </SectionContent>
+        </>
     );
 };
 
