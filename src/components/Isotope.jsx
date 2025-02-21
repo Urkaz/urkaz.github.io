e173e0a8a04351b581b932ace805212cf2c07d0d
Urkaz <mrurkaz@gmail.com>
2025-02-21 04:18:47 +0100

    Isotope general css and possible secondary filter (wip)

--- src/components/Isotope.jsx
+++ src/components/Isotope.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import Isotope from "isotope-layout";
 
-const IsotopeGrid = ({ filters, items, GridComponent }) => {
+const IsotopeGrid = ({ filters, secondaryfilters, items, GridComponent }) => {
   const gridRef = useRef(null);
   const [isotope, setIsotope] = useState(null);
   const [filter, setFilter] = useState("*");
+  const [secondaryfilter, setSecondaryFilter] = useState("*");
   const [gameIds] = useState(() =>
     items.map(() => crypto.randomUUID())
   );
@@ -12,7 +13,7 @@ const IsotopeGrid = ({ filters, items, GridComponent }) => {
   useEffect(() => {
     if (gridRef.current) {
       const iso = new Isotope(gridRef.current, {
-        itemSelector: ".grid-item",
+        itemSelector: ".isotope-grid-item",
         layoutMode: "fitRows",
       });
       setIsotope(iso);
@@ -31,13 +32,25 @@ const IsotopeGrid = ({ filters, items, GridComponent }) => {
     <div className="isotope-layout" data-default-filter="*" data-layout="masonry" data-sort="original-order">
       <div>
         {/* Botones para filtrar */}
-        <ul className="isotope-filters" data-aos="fade-up" data-aos-delay="100">
-          {filters.map((f) => (
-            <li key={f.name} onClick={() => setFilter(f.selector)} className={filter === f.selector ? "filter-active" : "filter-inactive"}>
-              {f.name}
-            </li>
-          ))}
-        </ul>
+        {filters ?
+          <ul className="isotope-filters" data-aos="fade-up" data-aos-delay="100">
+            {filters.map((f) => (
+              <li key={f.name} onClick={() => setFilter(f.selector)} className={filter === f.selector ? "filter-active" : "filter-inactive"}>
+                {f.name}
+              </li>
+            ))}
+          </ul>
+          : null}
+        {secondaryfilters ?
+          <ul className="isotope-filters" data-aos="fade-up" data-aos-delay="100">
+            {secondaryfilters.map((f) => (
+              <li key={f.name} onClick={() => setSecondaryFilter(f.selector)} className={secondaryfilter === f.selector ? "filter-active" : "filter-inactive"}>
+                {f.name}
+              </li>
+            ))}
+          </ul>
+          : null}
+
 
         {/* Grid de elementos con un componente dinámico */}
         <div ref={gridRef} className="row gy-4 isotope-container" data-aos="fade-up" data-aos-delay="200">
