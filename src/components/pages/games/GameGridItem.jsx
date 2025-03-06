81012d97a9ea72e083a9f8983a57d1043a70c471
Urkaz <mrurkaz@gmail.com>
2025-03-07 00:17:09 +0100

    Use current route as base for tile links

--- src/components/pages/games/GameGridItem.jsx
+++ src/components/pages/games/GameGridItem.jsx
@@ -2,21 +2,24 @@
 
 import React from "react";
 import Link from "next/link";
+import { usePathname } from 'next/navigation'
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faLink, faArrowUpRightFromSquare, faArrowTurnUp } from "@fortawesome/free-solid-svg-icons";
 
 import { cleanText } from "@src/components/functions";
 
 import styles from "@styles/pages/games.module.scss";
 
-export const GameGridItem = ({ category, name, tag, noLogo = false, platforms, hasSection = false, keyName }) => {
+export const GameGridItem = ({ category, name, tag, noLogo = false, platforms, hasSection = false, keyName, customRoute }) => {
+    const pathname = usePathname()
 
     const content = <>
         <div className={`${styles['game-content']} h-100 ${keyName} ${hasSection ? styles["selectable"] : null}`}>
-            {hasSection ?
-                <div className={styles['fold']}><div className={styles['fold-link']}><FontAwesomeIcon icon={faLink} /></div></div>
-                : null}
+            {hasSection || customRoute ?
+                <div className={styles['fold']}><div className={styles['fold-link']}><FontAwesomeIcon icon={customRoute ? faArrowTurnUp : faLink} /></div></div>
+                : null
+            }
             <div className={styles['game-overlay']}>
                 {noLogo == true ? <p>{name}</p> : <img src={`/img/games/logos/${cleanText(name)}.png`} className="img-fluid" alt={name} />}
             </div>
@@ -35,7 +38,7 @@ export const GameGridItem = ({ category, name, tag, noLogo = false, platforms, h
 
     return <>
         <div className={`col-lg-4 col-md-6 ${styles['small-grid-item']} isotope-grid-item ${category?.map(cat => `category-${cat}`).join(" ")} ${platforms?.map(platform => `category-${platform}`).join(" ")}`}>
-            {hasSection ? <Link href={`/games/${keyName}`}>{content}</Link> : content}
+            {hasSection || customRoute ? <Link href={customRoute ? customRoute : `${pathname}/${keyName}`}>{content}</Link> : content}
         </div>
     </>
 };
