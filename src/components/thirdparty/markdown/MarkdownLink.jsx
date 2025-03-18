5a5e80354227c5e1c77dfefac46f2bd2f700e1b8
Urkaz <mrurkaz@gmail.com>
2025-03-18 16:27:44 +0100

    Open Markdown links in a new tab

--- src/components/thirdparty/markdown/MarkdownLink.jsx
+++ src/components/thirdparty/markdown/MarkdownLink.jsx
@@ -1,8 +1,9 @@
 import { YoutubeVideo } from "@components/thirdparty/YoutubeVideo";
+import Link from "next/link";
 
 export const MarkdownLink = ({ children, href }) => {
     if (href.includes("youtube.com") || href.includes("youtu.be")) {
         return <YoutubeVideo videoURL={href} />;
     }
-    return <a href={href}>{children}</a>;
+    return <Link href={href} target="_blank" rel="noopener noreferrer">{children}</Link>;
 }
