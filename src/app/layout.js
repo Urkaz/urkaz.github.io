c6d4961dbb6cbfb6f9e1efde1d3b3d6bb12ecf57
Urkaz <mrurkaz@gmail.com>
2025-09-02 18:42:24 +0200

    Update dependancies + add Google Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,9 @@ import { ScrollToTop } from "@components/common/ScrollToTop";
 import { DottedBackground } from "@components/common/DottedBackground";
 import { BootstrapClient } from "@components/thirdparty/BootstrapClient";
 import { AOSClient } from "@components/thirdparty/AOSClient";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-232NTM50DL");
 
 // The following import prevents a Font Awesome icon server-side rendering bug,
 // where the icons flash from a very large icon down to a properly sized one:
