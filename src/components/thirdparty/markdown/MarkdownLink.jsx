6b8c12dca209df4f24383f7d2d5533bfc91fa278
Urkaz <mrurkaz@gmail.com>
2025-09-19 22:28:56 +0200

    Youtube video in Markdown changes to the same design as the Gallery

--- src/components/thirdparty/markdown/MarkdownLink.jsx
+++ src/components/thirdparty/markdown/MarkdownLink.jsx
@@ -1,9 +1,15 @@
 import { YoutubeVideo } from "@components/thirdparty/YoutubeVideo";
 import Link from "next/link";
 
+import styles from "@styles/components/thirdparty/custom_markdown.module.scss";
+
 export const MarkdownLink = ({ children, href }) => {
     if (href.includes("youtube.com") || href.includes("youtu.be")) {
-        return <YoutubeVideo videoURL={href} />;
+        return <span className="row">
+            <span className={`col-lg-8 ${styles["centered-gallery"]}`}>
+                <YoutubeVideo videoURL={href}/>
+            </span>
+        </span>;
     }
     return <Link href={href} target="_blank" rel="noopener noreferrer">{children}</Link>;
 }
