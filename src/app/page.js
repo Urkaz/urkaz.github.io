2785ce231a2a821f24ea130e998a426cacc45d13
Urkaz <mrurkaz@gmail.com>
2025-11-27 02:32:45 +0100

    Small rewrite to the home text

--- src/app/page.js
+++ src/app/page.js
@@ -16,34 +16,28 @@ export default function Home() {
                 <div className="row">
                     <div className="col-lg-6" data-aos="fade-up" data-aos-delay="100">
                         <h2>Fran Sánchez Rodrigo</h2>
-                        <p className={styles["sub-title"]}>Video game Programmer</p>
+                        <p className={styles["sub-title"]}>Unreal Engine Tools, Engine & Optimization Engineer</p>
                         <div className={styles["description"]}>
                             <p>
-                                Hello and welcome to my porfolio! My name is Fran Sánchez Rodrigo, and I&apos;m currently{" "}
+                                Hello and welcome to my portfolio!
+                            </p>
+                            <p>
+                                My name is Fran Sánchez Rodrigo, and I am currently{" "}
                                 <span className={styles["sub"]}>open to new opportunities and offers</span> for{" "}
-                                <span>core game systems programmer</span>, <span>tools programmer</span>, <span>porting engineer</span>, and more!
+                                <span>Unreal Engine</span> <span>tools programmer</span>, <span>engine programmer</span>,{" "}
+                                <span>porting engineer</span>, and more!
                             </p>
                             <p>
-                                I&apos;m graduated in Degree in Video game Design and Development and in the Master&apos;s Degree in Video Game
-                                Programming.
-                            </p>
-                            <p>
-                                With <span className={styles["sub"]}>+3 years of professional experience</span> in <span>videogame programming</span>,
+                                I hold a Bachelor&apos;s in Video Game Design and Development and a Master&apos;s in Video Game Programming.
+                                With <span className={styles["sub"]}>more than 3 years of professional experience</span> in <span>videogame programming</span>{" "}
                                 and <span className={styles["sub"]}>4 years of experience</span> as <span>QA Technician</span>, I bring 7 years of
-                                experience in the game industry ready to face any challenge!
+                                experience in the game industry and am ready to face any challenge!
                             </p>
                             <p>
-                                In this porfolio you will find an extensive explanation of all my personal and professional projects, experience, and
-                                formation.
+                                I&apos;m {age} years old, a passionate, adaptable, and detail-oriented programmer,{" "}
+                                lifelong video game enthusiast, and always eager to tackle new challenges.
                             </p>
-                        </div>
-                        <p className={styles["sub-title"]}>More about me</p>
-                        <div className={styles["description"]}>
                             <p>
-                                I&apos;m {age} years old, still a video game enthusiast and a passionate programmer, fast learner, and self-hosted
-                                sysadmin. Some of my Google Spreadsheets are probably more complex than the ones used in accounting companies.
-                                <br />
-                                <br />
                                 <LinearGradient gradient={["to bottom", "#fff100 ,#d96b17"]} className={styles["digimon"]}>
                                     DigiDestinied
                                 </LinearGradient>{" "}
