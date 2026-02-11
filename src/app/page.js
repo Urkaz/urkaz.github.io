6ab488b41ed9d3edcd1a9ada3bfd781904ad8271
Urkaz <mrurkaz@gmail.com>
2026-02-11 02:06:38 +0100

    Home changes

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import { LinkPills, LinkPill } from "@components/common/LinkPills";
 import styles from "@styles/pages/home.module.scss";
 
 export default function Home() {
-    var diff_ms = new Date().getTime() - new Date(Date.UTC(1994, 1, 12)).getTime(); //Future date - current date
+    var diff_ms = new Date().getTime() - new Date(Date.UTC(1994, 1, 12)).getTime();
     var age_dt = new Date(diff_ms);
     var age = Math.abs(age_dt.getUTCFullYear() - 1970);
 
@@ -22,20 +22,19 @@ export default function Home() {
                                 Hello and welcome to my portfolio!
                             </p>
                             <p>
-                                My name is Fran Sánchez Rodrigo, and I am currently{" "}
+                                My name is Fran S&aacute;nchez Rodrigo, and I&apos;m currently{" "}
                                 <span className={styles["sub"]}>open to new opportunities and offers</span> for{" "}
                                 <span>Unreal Engine</span> <span>tools programmer</span>, <span>engine programmer</span>, and {" "}
                                 <span>porting engineer</span>.
                             </p>
                             <p>
-                                I hold a Bachelor&apos;s in Video Game Design and Development and a Master&apos;s in Video Game Programming.
                                 With <span className={styles["sub"]}>more than 3 years of professional experience</span> in <span>videogame programming</span>{" "}
                                 and <span className={styles["sub"]}>4 years of experience</span> as <span>QA Technician</span>, I bring 7 years of
-                                experience in the game industry and am ready to face any challenge!
+                                experience in the video game industry.
                             </p>
                             <p>
                                 I&apos;m {age} years old, a passionate, adaptable, and detail-oriented programmer,{" "}
-                                lifelong video game enthusiast, and always eager to tackle new challenges.
+                                lifelong video game enthusiast, and always and am ready to face any challenge!
                             </p>
                             <p>
                                 <LinearGradient gradient={["to bottom", "#fff100 ,#d96b17"]} className={styles["digimon"]}>
