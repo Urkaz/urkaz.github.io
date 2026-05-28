297bd6ec5b82f74a3344e37149fd3cb65e502e5a
Urkaz <mrurkaz@gmail.com>
2026-05-28 02:09:23 +0200

    Register c++ code highlighter

--- src/components/thirdparty/markdown/MarkdownCodeBlock.jsx
+++ src/components/thirdparty/markdown/MarkdownCodeBlock.jsx
@@ -5,11 +5,13 @@ import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import ini from "react-syntax-highlighter/dist/esm/languages/prism/ini";
 import xml from "react-syntax-highlighter/dist/esm/languages/prism/xml-doc";
 import json from "react-syntax-highlighter/dist/esm/languages/prism/json";
+import cpp from "react-syntax-highlighter/dist/esm/languages/prism/cpp";
 
 // Register
 SyntaxHighlighter.registerLanguage("ini", ini);
 SyntaxHighlighter.registerLanguage("xml", xml);
-SyntaxHighlighter.registerLanguage("xml", json);
+SyntaxHighlighter.registerLanguage("json", json);
+SyntaxHighlighter.registerLanguage("cpp", cpp);
 
 export const MarkdownCodeBlock = ({ children, className, node, ...rest }) => {
     const match = /language-(\w+)/.exec(className || '');
