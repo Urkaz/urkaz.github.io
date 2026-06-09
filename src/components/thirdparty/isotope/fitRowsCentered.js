071787c4409888a59a0767d25ddcc668cb899e09
Urkaz <mrurkaz@gmail.com>
2026-06-09 04:34:43 +0200

    Fix broken isotope

--- src/components/thirdparty/isotope/fitRowsCentered.js
+++ src/components/thirdparty/isotope/fitRowsCentered.js
@@ -1,71 +1,75 @@
-let registered = false;
+let registrationPromise = null;
 
-export async function registerFitRowsCentered() {
-    if (registered) return;
-    registered = true;
+export function registerFitRowsCentered() {
+    if (registrationPromise) return registrationPromise;
 
-    const LayoutMode = (await import("isotope-layout/js/layout-mode")).default;
-    const FitRowsCentered = LayoutMode.create("fitRowsCentered");
-    const proto = FitRowsCentered.prototype;
+    registrationPromise = (async () => {
+        const Isotope = (await import("isotope-layout")).default;
+        const LayoutMode = Isotope.LayoutMode;
+        const FitRowsCentered = LayoutMode.create("fitRowsCentered");
+        const proto = FitRowsCentered.prototype;
 
-    proto._resetLayout = function () {
-        this.x = 0;
-        this.y = 0;
-        this.maxY = 0;
-        this._getMeasurement("gutter", "outerWidth");
-        this.centerX = [];
-        this.currentRow = 0;
-        this.initializing = true;
-
-        const items = this.isotope.filteredItems;
-        for (let i = 0; i < items.length; i++) {
-            this._getItemLayoutPosition(items[i]);
-        }
-
-        if (this.centerX[this.currentRow]) {
-            this.centerX[this.currentRow].offset =
-                (this.isotope.size.innerWidth + this.gutter - this.x) / 2;
-        }
-
-        this.initializing = false;
-        this.currentRow = 0;
-        this.x = 0;
-        this.y = 0;
-        this.maxY = 0;
-        this._getMeasurement("gutter", "outerWidth");
-    };
-
-    proto._getItemLayoutPosition = function (item) {
-        item.getSize();
-        const itemWidth = item.size.outerWidth + this.gutter;
-        const containerWidth = this.isotope.size.innerWidth + this.gutter;
-
-        if (this.x !== 0 && itemWidth + this.x > containerWidth) {
-            if (this.initializing) {
-                this.centerX[this.currentRow].offset = (containerWidth - this.x) / 2;
-            }
-            this.currentRow++;
+        proto._resetLayout = function () {
             this.x = 0;
-            this.y = this.maxY;
-        }
+            this.y = 0;
+            this.maxY = 0;
+            this._getMeasurement("gutter", "outerWidth");
+            this.centerX = [];
+            this.currentRow = 0;
+            this.initializing = true;
 
-        if (this.initializing && this.x === 0) {
-            this.centerX.push({ offset: 0 });
-        }
+            const items = this.isotope.filteredItems;
+            for (let i = 0; i < items.length; i++) {
+                this._getItemLayoutPosition(items[i]);
+            }
 
-        const offset = this.centerX[this.currentRow]?.offset ?? 0;
-        const position = {
-            x: this.x + (this.initializing ? 0 : offset),
-            y: this.y,
+            if (this.centerX[this.currentRow]) {
+                this.centerX[this.currentRow].offset =
+                    (this.isotope.size.innerWidth + this.gutter - this.x) / 2;
+            }
+
+            this.initializing = false;
+            this.currentRow = 0;
+            this.x = 0;
+            this.y = 0;
+            this.maxY = 0;
+            this._getMeasurement("gutter", "outerWidth");
         };
 
-        this.maxY = Math.max(this.maxY, this.y + item.size.outerHeight);
-        this.x += itemWidth;
+        proto._getItemLayoutPosition = function (item) {
+            item.getSize();
+            const itemWidth = item.size.outerWidth + this.gutter;
+            const containerWidth = this.isotope.size.innerWidth + this.gutter;
 
-        return position;
-    };
+            if (this.x !== 0 && itemWidth + this.x > containerWidth) {
+                if (this.initializing) {
+                    this.centerX[this.currentRow].offset = (containerWidth - this.x) / 2;
+                }
+                this.currentRow++;
+                this.x = 0;
+                this.y = this.maxY;
+            }
 
-    proto._getContainerSize = function () {
-        return { height: this.maxY };
-    };
+            if (this.initializing && this.x === 0) {
+                this.centerX.push({ offset: 0 });
+            }
+
+            const offset = this.centerX[this.currentRow]?.offset ?? 0;
+            const position = {
+                x: this.x + (this.initializing ? 0 : offset),
+                y: this.y,
+            };
+
+            this.maxY = Math.max(this.maxY, this.y + item.size.outerHeight);
+            this.x += itemWidth;
+
+            return position;
+        };
+
+        proto._getContainerSize = function () {
+            return { height: this.maxY };
+        };
+    })();
+
+    return registrationPromise;
 }
